6e024988842a7c414571972bcc29bc6cae1af6af
gpongelli <gpongelli@users.noreply.github.com>
2023-01-13 22:45:05 +0100

    fix: remove unused tests

--- tests/test_python_active_versions.py
+++ tests/test_python_active_versions.py
@@ -6,42 +6,7 @@
 
 """Tests for `python_active_versions` package."""
 
-import pytest
-from click.testing import CliRunner
-
-from python_active_versions.cli_tools import cli
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-
-    Arguments:
-        response: pytest feature
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'python-active-versions' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+# import pytest
 
 
 def test_py_version():
